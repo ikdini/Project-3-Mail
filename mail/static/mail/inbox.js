5a8310dd7c0e7fc93416238ea9a8619eccead017
Ikdini <ioluwande@gmail.com>
2022-05-14 22:39:59 +0100

    edit

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -26,16 +26,20 @@ function compose_email(email) {
 }
 
 function prefill_subject(subject) {
-  const prefix = "Re: "
+ /* Checking if the subject line of the email being replied to has the prefix "Re: " and if it does
+ not, it adds it. */
+  const prefix = "Re: ";
   if (subject.slice(0,4) === prefix) {
-    return subject
+    return subject;
   } else {
-    return prefix + subject
+    return prefix + subject;
   }
 }
 
 function prefill_body(email) {
-  return `On ${email.timestamp} ${email.sender} wrote: ${email.body}\n`;
+  /* This is a template literal that is used to prefill the body of the email with the timestamp,
+  sender, and body of the email being replied to. */
+  return `On ${email.timestamp} ${email.sender} wrote: ${email.body}`;
 }
 
 function submit_mail() {
@@ -75,14 +79,21 @@ function add_mail(email) {
 
   mailbox = this.valueOf();
 
+  /* This is creating a new div element and appending it to the emails-view div. */
   const inner = `<div class="row mb-0"><div class="col-md-8"><em>${email.sender}</em></div><div class="col-md-4 email-timestamp"><span>${email.timestamp}</span></div></div><span class="h5">${email.subject.charAt(0).toUpperCase() + email.subject.slice(1)}</span>`;
   const mail_entry = document.createElement("div");
   mail_entry.className = "email-entry";
   mail_entry.innerHTML = inner;
   document.querySelector('#emails-view').append(mail_entry);
   
+  /* This is a ternary operator that is setting the background color of the email entry to grey if the
+  email has been read and white if it has not been read. */
   mail_entry.style.background = email.read ? 'grey' : 'white';
 
+/* This is an event listener that is listening for a click on the email entry. When the email entry is
+clicked, it is making a PUT request to the server to update the read status of the email to true. It
+is then making a GET request to the server to get the email and then calling the view_mail function
+to display the email. */
   mail_entry.onclick = function () {
     
     fetch(`/emails/${email.id}`, {
@@ -109,9 +120,6 @@ function view_mail(email, mailbox) {
   document.querySelector('#email-entry-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
-  // const email_view = document.createElement('div');
-  // email_view.className = 'email-viewer';
-  
   const sender = `<p><b>From: </b>${email.sender}</p>`;
   const recipients = `<p><b>To: </b>${email.recipients}</p>`;
   const subject = `<p><b>Subject: </b>${email.subject.charAt(0).toUpperCase() + email.subject.slice(1)}</p>`;
@@ -119,12 +127,8 @@ function view_mail(email, mailbox) {
   const body = `<hr><p>${email.body}</p>`;
   const reply = '<button class="btn btn-sm btn-outline-primary" id="rep">Reply</button>';
 
-  // email_view.innerHTML = `<p><b>From: </b>${email.sender}</p>
-  //                         <p><b>To: </b>${email.recipients}</p>
-  //                         <p><b>Subject: </b>${email.subject}</p>
-  //                         <p><b>Timestamp: </b>${email.timestamp}</p>`;
-                          
-  // document.querySelector('#email-entry-view').append(email_view);
+  /* This is setting the innerHTML of the email-entry-view div to the sender, recipients, subject,
+  timestamp, and reply button. */
   document.querySelector('#email-entry-view').innerHTML = sender + recipients + subject + timestamp + reply;
   
   const arc_btn_txt = email.archived ? "Unarchive" : "Archive";
@@ -136,16 +140,16 @@ function view_mail(email, mailbox) {
   document.querySelector('#email-entry-view').innerHTML += body;
   
   if (mailbox !== 'sent') {
-    // document.querySelector("#arc").onclick = toggle_archive(email);
     document.querySelector('#arc').addEventListener('click', () => toggle_archive(email));
   }
 
   document.querySelector('#rep').addEventListener('click', () => compose_email(email));
-  // document.querySelector('#rep').addEventListener('click', () => compose_email);
 }
 
 function toggle_archive(email) {
 
+  /* This is making a PUT request to the server to update the archived status of the email to the
+  opposite of what it currently is. It is then reloading the page. */
   fetch(`/emails/${email.id}`, {
     method: 'PUT',
     body: JSON.stringify({
